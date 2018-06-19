5d7cfe8c8c4bb96c68f6ccbb6c7d65c5e1e4cae5
StillTravelling <breakthroughTGW@gmail.com>
2018-06-19 19:50:29 +0800

    update form.js
    add checkbox, select and textarea demo.

--- src/components/form.js
+++ src/components/form.js
@@ -3,12 +3,29 @@ import React from "react";
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = {
+      name: "lsq",
+      gender: "boy",
+      favouriteDrink: "coffee",
+      intro: "a boy who loves programming."
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleGenderChange = this.handleGenderChange.bind(this);
+    this.handleFdChange = this.handleFdChange.bind(this);
+    this.handleIntroChange = this.handleIntroChange.bind(this);
+  }
+  handleIntroChange(event) {
+    this.setState({ intro: event.target.value });
   }
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ name: event.target.value });
+  }
+  handleFdChange(event) {
+    this.setState({ favouriteDrink: event.target.value });
+  }
+  handleGenderChange(event) {
+    this.setState({ gender: event.target.value });
   }
   handleSubmit(event) {
     alert(event.target);
@@ -17,12 +34,54 @@ class Form extends React.Component {
   render() {
     return (
       <form>
-        <input
-          name="name"
-          onChange={this.handleChange}
-          value={this.state.value}
-        />
-        <p>{this.state.value}</p>
+        <div>
+          name:
+          <input
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.value}
+            className="form-control"
+          />
+        </div>
+        <div>
+          gender: boy:
+          <input
+            value="boy"
+            type="radio"
+            name="gender"
+            onChange={this.handleGenderChange}
+          />
+          girl:
+          <input
+            value="girl"
+            type="radio"
+            name="gender"
+            onChange={this.handleGenderChange}
+          />
+        </div>
+        favourite drink:
+        <select
+          value={this.state.favouriteDrink}
+          onChange={this.handleFdChange}
+        >
+          <option value="" />
+          <option value="tea">tea</option>
+          <option value="coffee">coffee</option>
+          <option value="milk">milk</option>
+        </select>
+        <div />
+        <div>
+          introduction:{" "}
+          <textarea
+            value={this.state.intro}
+            onChange={this.handleIntroChange}
+          />
+        </div>
+        <p>
+          {this.state.name} is a {this.state.gender} and he likes{" "}
+          {this.state.favouriteDrink}
+        </p>
+        <p>Self Introduction: {this.state.intro}</p>
       </form>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Clock from "./components/clock";
 import FilterTable from "./components/filterTable";
+import Form from "./components/form";
 
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<FilterTable />, rootElement);
+ReactDOM.render(<Form />, rootElement);
